70a29239868e577aae0859ffa53594917f4d2f6f
opanelif1 <64418263+opanelif1@users.noreply.github.com>
2020-08-14 03:11:27 +0300

    Proje dosyası ekle.

---
+++ typeofoperator1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace typeofoperator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(typeof(int));
+            Console.WriteLine(typeof(byte));
+            Console.WriteLine(typeof(double));
+            Console.WriteLine(typeof(float));
+            Console.WriteLine(typeof(char));
+
+            Console.ReadLine();
+        }
+    }
+}
